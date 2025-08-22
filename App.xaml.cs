139e76fec080a42ad30d395eeaae9b5ab565637f
Matheus4ndrade <matheus014ndrade@gmail.com>
2025-08-22 18:29:32 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HeatInputCalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
